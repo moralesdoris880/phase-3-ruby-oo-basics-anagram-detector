e56c08b11efbee35f4a53e5c98290d981311e93a
Doris Morales <moralesdoris880@gmail.com>
2022-02-09 14:26:52 -0500

    commit tree 444230c88e8a7684dcc5753c7a160f3f364c1060

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+class Anagram
+    def initialize(word)
+        @word=word
+    end
+    def match(array)
+        new_word=@word.chars
+        anagramwords=[]
+        for i in array do
+            comp_word=i.chars
+            if comp_word.sort == new_word.sort
+                anagramwords.push(i)
+            end
+            
+        end
+        return anagramwords
+    end
+end
